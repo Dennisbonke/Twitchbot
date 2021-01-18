7d1909ac3191e96942430049ce0fa2276a23f1ee
Westlanderz <noah28129912@gmail.com>
2021-01-18 23:23:44 +0100

    Removed helpmessage

--- src/utils/command.ts
+++ src/utils/command.ts
@@ -2,7 +2,6 @@ import CommandContext from './commandContext';
 
 export default interface Command {
     readonly commandNames: string[];
-    getHelpMessage (commandPrefix: string): string;
     run (parsedUserCommand: CommandContext): Promise<void>;
     hasPermissionToRun (parsedUserCommand: CommandContext): boolean;
 }
