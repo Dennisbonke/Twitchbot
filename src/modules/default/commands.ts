8a8f3f3825e4a2a62915e05ae60097bf8d9f469a
Westlanderz <noah28129912@gmail.com>
2021-01-18 23:23:32 +0100

    Partial fix to commandlister

--- src/modules/default/commands.ts
+++ src/modules/default/commands.ts
@@ -3,19 +3,20 @@ import Command from '../../utils/command';
 import { client } from '../../bot';
 
 export class ListCommands implements Command {
-    readonly commandNames = ['help', 'halp', 'hlep'];
+    readonly commandNames = ['commands', 'help'];
 
     private commands: Command[];
 
     constructor (commands: Command[]) {
-        this.commands = commands;
+        this.commands = commands;     
     }
 
     async run (commandContext: CommandContext): Promise<void> {
-        const allowedCommands = this.commands.filter((command) =>
-            command.hasPermissionToRun(commandContext),
-        );
-
+        const allowedCommands = this.commands.filter((command) => {     
+            command.hasPermissionToRun(commandContext)       
+        });
+        
+        
         if (commandContext.args.length === 0) {
             // No command specified, give the user a list of all commands they can use.
             const commandNames = allowedCommands.map(
@@ -24,7 +25,7 @@ export class ListCommands implements Command {
             await client.say(client.channels[0],
                 `here is a list of commands you can run: ${commandNames.join(
                     ', ',
-                )}. Try !help ${commandNames[0]} to learn more about one of them.` +
+                )}. Try !commands ${commandNames[0]} to learn more about one of them.` +
                 '\nVersion: 0.1 https://github.com/Westlanderz/DiscordBot',
             );
             return;
@@ -33,31 +34,18 @@ export class ListCommands implements Command {
         const matchedCommand = this.commands.find((command) =>
             command.commandNames.includes(commandContext.args[0]),
         );
+        //console.log(matchedCommand);
+        
         if (!matchedCommand) {
             await client.say(client.channels[0],
-                "I don't know about that command :(. Try !help to find all commands you can use.",
+                "I don't know about that command :(. Try !commands to find all commands you can use.",
             );
             throw Error('Unrecognized command');
         }
-        if (allowedCommands.includes(matchedCommand)) {
-            await client.say(client.channels[0],
-                this.buildHelpMessageForCommand(matchedCommand, commandContext),
-            );
-        }
-    }
-
-    private buildHelpMessageForCommand (command: Command, context: CommandContext): string {
-        return `${command.getHelpMessage(
-            context.commandPrefix,
-        )}\nCommand aliases: ${command.commandNames.join(', ')}`;
     }
 
     hasPermissionToRun (commandContext: CommandContext): boolean {
         return true;
-    }
-
-    getHelpMessage (commandPrefix: string) {
-        return 'I think you already know how to use this command...';
     }
 }
 
