f05b8571c5135671a206dea0978d83da3287d47d
Westlanderz <noah28129912@gmail.com>
2021-02-27 21:23:48 +0100

    Changed for the cpp bot

--- src/utils/command.ts
+++
@@ -1,7 +0,0 @@
-import CommandContext from './commandContext';
-
-export default interface Command {
-    readonly commandNames: string[];
-    run (parsedUserCommand: CommandContext): Promise<void>;
-    hasPermissionToRun (parsedUserCommand: CommandContext): boolean;
-}
