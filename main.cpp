16c5d42d8de11d4edd861c901b7228c113033858
Westlanderz <noah28129912@gmail.com>
2021-02-28 11:54:03 +0100

    Changed to read oauth from a file so there is no oauth code on github

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <sockpp/tcp_connector.h>
+#include <fstream>
 #include "includes/bot.hpp"
 
 int main(void) {
@@ -10,10 +11,28 @@ int main(void) {
     sockpp::tcp_connector *conn = new sockpp::tcp_connector(sockpp::inet_address(host, port));
     
     std::cout << "connected to " << sockpp::inet_address(host, port) << std::endl;
-    
+
+    std::fstream config_file;
+    std::string oauthcode;
+    config_file.open("config.txt", std::ios::in);
+	if (!config_file) {
+		std::cerr << "No such file" << std::endl;
+	} else {
+		char ch;
+
+		while (1) {
+			config_file >> ch;
+			if (config_file.eof())
+				break;
+            oauthcode.push_back(ch);
+		}
+	}
+	config_file.close();
+
     Bot *bot = new Bot("westlanderz", "westlanderz", "!", conn);
+
     try{
-        bot->log_in("");
+        bot->log_in(oauthcode);
         bot->run();
     } catch(std::exception &e) {
         std::cerr << "Exception thrown in main\r\n" << e.what() << std::endl;
