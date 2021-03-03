de457f79a264b92d4d390f6a53384ffc1d695654
Westlanderz <noah28129912@gmail.com>
2021-03-03 17:49:05 +0100

    Adding the edit command

--- src/commandhandler.cpp
+++ src/commandhandler.cpp
@@ -4,6 +4,7 @@
 #include "../includes/commands/changeprefix.hpp"
 #include "../includes/commands/lurkcommand.hpp"
 #include "../includes/commands/helpcommand.hpp"
+#include "../includes/commands/editresult.hpp"
 
 CommandHandler::CommandHandler(Bot *_bot) : bot{_bot} {
     init_command_list();
@@ -19,6 +20,7 @@ void CommandHandler::init_command_list() {
     available_commands.push_back(new PingCommand(bot));
     available_commands.push_back(new ChangePrefixCommand(bot));
     available_commands.push_back(new LurkCommand(bot));
+        // available_commands.push_back(new EditresultCommand(available_commands, bot));
     available_commands.push_back(new HelpCommand(available_commands, bot));
 }
 
@@ -29,8 +31,8 @@ void CommandHandler::search_command(std::string command, bool mod, bool sub, std
                 _command->execute(sender, original_message, mod, sub, channel);
             else
                 bot->send_chat_message(sender + " you dont have the permissions to run this command.", channel);
-        } else {
-            bot->send_chat_message(sender + " I do not recognise this command. Try !help to see a list of the commands.", channel);
+            return;
         }
     }
+    bot->send_chat_message(sender + " I do not recognise this command. Try !help to see a list of the commands.", channel);
 }
