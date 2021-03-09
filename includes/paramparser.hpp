6ecce48ffcb0c7ace7be18a51458b70c60cfe7bd
Westlanderz <noah28129912@gmail.com>
2021-03-09 23:40:32 +0100

    Added class nothing in it yet

---
+++ includes/paramparser.hpp
@@ -0,0 +1,25 @@
+#ifndef _paramparser_h
+#define _paramparser_h
+
+#include <vector>
+
+class ParamParser {
+    public:
+        ParamParser();
+        virtual ~ParamParser() = default;
+
+
+    private:
+        
+};
+
+#endif //_paramparser_h
+
+
+/**
+ *  ${sender} -> 
+ *  ${user} ->
+ *  ${count} -> 
+ * ${sender}
+ * ${sender}
+ */
