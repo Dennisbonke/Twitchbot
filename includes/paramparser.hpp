51e4e1618d8f1404d6c35dcc7fc0ecc5316d74bf
Westlanderz <noah28129912@gmail.com>
2021-03-10 17:38:00 +0100

    Added constructor and basic properties

--- includes/paramparser.hpp
+++ includes/paramparser.hpp
@@ -2,15 +2,17 @@
 #define _paramparser_h
 
 #include <vector>
+#include <string>
 
 class ParamParser {
     public:
         ParamParser();
         virtual ~ParamParser() = default;
 
+        std::string parse_command(std::string);
 
     private:
-        
+        std::vector<std::string> params;
 };
 
 #endif //_paramparser_h
@@ -20,6 +22,4 @@ class ParamParser {
  *  ${sender} -> 
  *  ${user} ->
  *  ${count} -> 
- * ${sender}
- * ${sender}
  */
---
+++ src/paramparser.cpp
@@ -0,0 +1,5 @@
+#include "../paramparser.hpp"
+
+ParamParser::ParamParser() {
+    
+}
