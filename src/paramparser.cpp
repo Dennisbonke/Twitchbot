f675a40710dc9dc40c9abb9c1bff84504e34faf4
Westlanderz <noah28129912@gmail.com>
2021-03-10 17:51:52 +0100

    Work it does not yet

--- src/paramparser.cpp
+++ src/paramparser.cpp
@@ -1,5 +1,13 @@
 #include "../paramparser.hpp"
 
 ParamParser::ParamParser() {
+    params.push_back("sender");
+    params.push_back("user");
+    params.push_back("count");
+}
+
+ParamParser::~ParamParser() {}
+
+std::string parse_command(std::string sender) {
     
 }
