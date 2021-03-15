1d159ced054417295c982c3d967b34c1d7672495
Westlanderz <noah28129912@gmail.com>
2021-03-15 17:10:18 +0100

    Got a basic verion working

--- includes/timerhandler.hpp
+++ includes/timerhandler.hpp
@@ -1,13 +1,23 @@
 #ifndef _timerhandler_h
 #define _timerhandler_h
 
+#include <string>
+
+class Bot;
+
 class TimerHandler {
     public:
-        TimerHandler();
+        TimerHandler(std::string, Bot *);
         virtual ~TimerHandler();
 
+        void calc_timer();
+        std::string is_timer_file();
+
     private:
-        
+        std::string timer_file;
+        std::string channel;
+        int amount_timers;
+        Bot *bot;
 };
 
 #endif
